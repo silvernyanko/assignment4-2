9b361c532ee2c356337cfac12546ab9f02c62cc4
silvernyanko <h8octf.jay.mcbz@gmail.com>
2023-10-13 22:51:00 +0900

    to lower camel case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        List<MoominCharacter> MoominValley = new ArrayList<MoominCharacter>();
-        MoominValley.add(new MoominCharacter("Moomintroll", "ムーミントロール"));
-        MoominValley.add(new MoominCharacter("Floren", "フローレン"));
-        MoominValley.add(new MoominCharacter("Moominpappa", "ムーミンパパ"));
-        MoominValley.add(new MoominCharacter("Moominmamma", "ムーミンママ"));
-        MoominValley.add(new MoominCharacter("Little My", "リトルミイ"));
-        MoominValley.add(new MoominCharacter("Snufkin", "スナフキン"));
-        MoominValley.add(new MoominCharacter("Snork", "スノーク"));
-        MoominValley.add(new MoominCharacter("Sniff", "スニフ"));
-        MoominValley.add(new MoominCharacter("Mimble", "ミムラ"));
-        MoominValley.add(new MoominCharacter("Hemulen", "ヘムレンさん"));
-        MoominValley.add(new MoominCharacter("Too-Ticky", "トゥーティッキ"));
-        MoominValley.add(new MoominCharacter("Stinky", "スティンキー"));
-        MoominValley.add(new MoominCharacter("the Groke", "モラン"));
-        MoominValley.add(new MoominCharacter("the Hattifatteners", "ニョロニョロ"));
+        List<MoominCharacter> moominValley = new ArrayList<MoominCharacter>();
+        moominValley.add(new MoominCharacter("Moomintroll", "ムーミントロール"));
+        moominValley.add(new MoominCharacter("Floren", "フローレン"));
+        moominValley.add(new MoominCharacter("Moominpappa", "ムーミンパパ"));
+        moominValley.add(new MoominCharacter("Moominmamma", "ムーミンママ"));
+        moominValley.add(new MoominCharacter("Little My", "リトルミイ"));
+        moominValley.add(new MoominCharacter("Snufkin", "スナフキン"));
+        moominValley.add(new MoominCharacter("Snork", "スノーク"));
+        moominValley.add(new MoominCharacter("Sniff", "スニフ"));
+        moominValley.add(new MoominCharacter("Mimble", "ミムラ"));
+        moominValley.add(new MoominCharacter("Hemulen", "ヘムレンさん"));
+        moominValley.add(new MoominCharacter("Too-Ticky", "トゥーティッキ"));
+        moominValley.add(new MoominCharacter("Stinky", "スティンキー"));
+        moominValley.add(new MoominCharacter("the Groke", "モラン"));
+        moominValley.add(new MoominCharacter("the Hattifatteners", "ニョロニョロ"));
 
-        for (int i = 0; i < MoominValley.size(); i++) {
-            if (MoominValley.get(i).getEnglishName().startsWith("M") ) {
-                System.out.println("名前：" + MoominValley.get(i).getJapaneseName());
+        for (int i = 0; i < moominValley.size(); i++) {
+            if (moominValley.get(i).getEnglishName().startsWith("M") ) {
+                System.out.println("名前：" + moominValley.get(i).getJapaneseName());
             }
         }
     }
